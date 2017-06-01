369b5883d945b92fe12121b8df79d1d4d2455b39
periman22 <periman@windowslive.com>
2017-06-01 04:06:34 +0300

    added randomize boxes

--- src/containers/game.js
+++ src/containers/game.js
@@ -10,6 +10,9 @@ class Game extends Component {
         this.renderPosition = this.renderPosition.bind(this);
         this.handleKeyDown = this.handleKeyDown.bind(this);
         this.buildRooms = this.buildRooms.bind(this);
+        this.checkOverlap = this.checkOverlap.bind(this);
+        this.getAllCorners = this.getAllCorners.bind(this);
+        this.findIfNotInside = this.findIfNotInside.bind(this);
     }
     renderPosition() {
         let ctx = this.getCanvas();
@@ -30,13 +33,18 @@ class Game extends Component {
         ctx.fillStyle = 'red';
         ctx.fill();
         ctx.closePath();
+        ctx.scale(2, 2)
+        ctx.translate(dim.x, dim.y)
+        
     }
     
     handleKeyDown(ev) {
+        const ctx = this.getCanvas();
         const key = ev.key;
         const position = this.props.playerPosition;
         //This will update according to velocit and acceleration in the future too
-        const updatePosition = this.updatePosition(position, 3, key);
+        ctx.clearRect((position.x - 5), (position.y - 5), 10, 10);
+        const updatePosition = this.updatePosition(position, 4, key);
         this.props.changePlayerPosition(updatePosition);
     }
     buildRooms(){
@@ -49,31 +57,93 @@ class Game extends Component {
         const height = this.props.gameInfo.canvasHeight;
         ctx.clearRect(0, 0, width, height);
         // innitiate with a random starting point;
-        let Start = {};
         
-        let density = 20;
+        let density = 150;
         for(let i = 0; i < density; i ++){
-            Start.X = this.getRandomDimention(width, 'start');
-            Start.Y = this.getRandomDimention(height, 'start');
             let newRoom = {}
             newRoom.boxWidth = this.getRandomDimention(width);
             newRoom.boxHeight = this.getRandomDimention(height);
             newRoom.X = this.getRandomDimention(width, 'start');
             newRoom.Y = this.getRandomDimention(height, 'start');
             if(rooms.length > 0){
-                let isOverlap = rooms.filter(this.checkOverlap(room, newRoom));
+                let isOverlap = this.checkOverlap(newRoom, rooms, width, height);
+                if(isOverlap){
+                    rooms.push(newRoom);
+                    this.drawBox(ctx, newRoom);
+                }
             } else {
                 //if there is no room craeted then create one
                 rooms.push(newRoom);
+                this.drawBox(ctx, newRoom);
             }
         }
-        console.log(rooms);
-        
-        // console.log(newRoom);
+        // console.log(rooms);        
     }
-    checkOverlap(oldRoom, newRoom){
-        console.log(oldRoom, newRoom);
-    }   
+    drawBox(ctx, room){
+        ctx.rect(room.X, room.Y, room.boxWidth, room.boxHeight);
+        ctx.stroke();
+    }
+    checkOverlap(newRoom, rooms, width, height){
+        let cornersOfNewRoom = this.getAllCorners(newRoom);
+        if(cornersOfNewRoom.corners.UR.X > width | cornersOfNewRoom.corners.DL.Y > height){
+            return false;
+        }
+        let NotOverlap = false;
+        for (let j = 0; j < rooms.length; j ++){
+            let cornersEl = this.getAllCorners(rooms[j]);
+            NotOverlap = this.findIfNotInside(cornersOfNewRoom, cornersEl);
+            if(!NotOverlap){
+                return false
+            }
+            // console.log(NotOverlap);
+            //NotOverlap is true when there is no overlap
+        };
+        return true;
+    }
+    findIfNotInside(room1, room2){
+        let c = room1.corners;
+        let c2 = room2.corners;
+        let lineChecker = this.checkForLines(c, c2);
+        if(lineChecker === false){
+            return false;
+        }
+        let lineChecker2 = this.checkForLines(c2, c);
+        if(lineChecker2 === false){
+            return false;
+        }
+        for (let corner in c){
+            let checker = this.checkOneCorner(c, c2, corner);
+            if(checker === false){
+                return false;
+            }
+            let checker2 = this.checkOneCorner(c2, c, corner);
+            if(checker2 === false){
+                return false;
+            }
+        }
+        return true;
+    }
+    checkForLines(c, c2){
+        if(( (c.DL.X >= c2.DL.X ) && (c.DR.X <= c2.DR.X)) && ((c2.UL.Y >= c.UL.Y ) && (c2.DL.Y <= c.DL.Y))){
+            return false;
+        }
+        return true;
+    }
+    checkOneCorner(c, c2, corner){
+        if(((c2.UL.X <= c[corner].X ) && (c[corner].X  <= (c2.UR.X))) && ((c2.UL.Y <= c[corner].Y ) && (c[corner].Y  <= (c2.DL.Y)))){
+            return false;
+        }
+        return true;
+    }
+    getAllCorners(room){
+        let corners = {};
+        corners.UL = {X: room.X, Y: room.Y};
+        corners.UR = {X: room.X + room.boxWidth, Y: room.Y};
+        corners.DR = {X: room.X + room.boxWidth, Y: room.Y + room.boxHeight};
+        corners.DL = {X: room.X, Y: room.Y + room.boxHeight};
+        room.corners = corners;
+        return room;
+    }
     getRandomDimention(dim, object){
         if(object === 'start'){
             return Math.floor(Math.random() * dim);
@@ -81,9 +151,6 @@ class Game extends Component {
         const randomize = 0.2 + Math.random() * 0.15;
         let newRoomDim = Math.ceil((0.05 + Math.random() * 0.15) * dim)
         return newRoomDim;
-    }
-    getVariance(dim){
-        return 0.2 * dim + dim;
     }
     getCanvas(){
         const canvas = document.getElementById("mycanvas");
