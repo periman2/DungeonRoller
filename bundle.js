1ce6a1250e7b3239ac05a64471c9f54df7503392
periman22 <periman@windowslive.com>
2017-06-16 10:24:41 +0300

    First commit again

--- bundle.js
+++ bundle.js
@@ -23014,8 +23014,8 @@
 	        screenHeight: 600,
 	        coredorWidth: 19,
 	        roomDensity: 2000,
-	        roomColor: 'https://dl.dropboxusercontent.com/s/jm3h1hvw32l7ur5/floor2.png?dl=0',
-	        corredorColor: 'https://dl.dropboxusercontent.com/s/jm3h1hvw32l7ur5/floor2.png?dl=0',
+	        roomColor: 'https://dl.dropboxusercontent.com/s/jm3h1hvw32l7ur5/floor2.png?dl=1',
+	        corredorColor: 'https://dl.dropboxusercontent.com/s/jm3h1hvw32l7ur5/floor2.png?dl=1',
 	        radius: 6,
 	        initialVel: 1.3,
 	        trapColor: 'https://photos-1.dropbox.com/t/2/AADRe1gxQV5q4y6ozskZTb1pj8I0JYtT6zIFmkt5rl8Gmw/12/596040564/png/32x32/1/_/1/2/blackHole.png/EKXCgecEGKcBIAIoAg/IjEa4oQGBWNjcf5NHLD3K-os1Cfd0Drbdu7hrDXy67E?size=1600x1200&size_mode=3'
