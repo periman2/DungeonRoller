226cfef38d164646ff4555b71ee15b8fcc4da2cf
periman22 <periman@windowslive.com>
2017-06-16 10:23:52 +0300

    First commit again

--- bundle.js
+++ bundle.js
@@ -23014,8 +23014,8 @@
 	        screenHeight: 600,
 	        coredorWidth: 19,
 	        roomDensity: 2000,
-	        roomColor: 'https://photos-5.dropbox.com/t/2/AAAi4Tm8THd0iSH3F0JuDRSoJAniZynJMzxj1tnzHj6bLA/12/596040564/png/32x32/1/_/1/2/floor2.png/EKXCgecEGJsBIAIoAg/Vv3M1IGbWw46V-GaOoXOyECmsd7zpAL5FDCRa1aR2_E?size=1600x1200&size_mode=3',
-	        corredorColor: 'https://photos-5.dropbox.com/t/2/AAAi4Tm8THd0iSH3F0JuDRSoJAniZynJMzxj1tnzHj6bLA/12/596040564/png/32x32/1/_/1/2/floor2.png/EKXCgecEGJsBIAIoAg/Vv3M1IGbWw46V-GaOoXOyECmsd7zpAL5FDCRa1aR2_E?size=1600x1200&size_mode=3',
+	        roomColor: 'https://dl.dropboxusercontent.com/s/jm3h1hvw32l7ur5/floor2.png?dl=0',
+	        corredorColor: 'https://dl.dropboxusercontent.com/s/jm3h1hvw32l7ur5/floor2.png?dl=0',
 	        radius: 6,
 	        initialVel: 1.3,
 	        trapColor: 'https://photos-1.dropbox.com/t/2/AADRe1gxQV5q4y6ozskZTb1pj8I0JYtT6zIFmkt5rl8Gmw/12/596040564/png/32x32/1/_/1/2/blackHole.png/EKXCgecEGKcBIAIoAg/IjEa4oQGBWNjcf5NHLD3K-os1Cfd0Drbdu7hrDXy67E?size=1600x1200&size_mode=3'
