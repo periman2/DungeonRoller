dcbb04353663d16b38f30240f793950c5bf08a0d
periman22 <periman@windowslive.com>
2017-06-16 10:26:20 +0300

    First commit again

--- src/reducers/game-info.js
+++ src/reducers/game-info.js
@@ -10,6 +10,6 @@ export default function() {
         corredorColor: 'https://dl.dropboxusercontent.com/s/jm3h1hvw32l7ur5/floor2.png?dl=1',
         radius: 6,
         initialVel: 1.3,
-        trapColor: 'https://photos-1.dropbox.com/t/2/AADRe1gxQV5q4y6ozskZTb1pj8I0JYtT6zIFmkt5rl8Gmw/12/596040564/png/32x32/1/_/1/2/blackHole.png/EKXCgecEGKcBIAIoAg/IjEa4oQGBWNjcf5NHLD3K-os1Cfd0Drbdu7hrDXy67E?size=1600x1200&size_mode=3'
+        trapColor: 'https://dl.dropboxusercontent.com/s/opbei51tsw3toe3/blackHole.png?dl=1'
     }
 }
